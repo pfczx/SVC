18a9574ee989d635ccc8c63e4c1e6c085c5751ce
pfczx <marcel.ras@edu.uekat.pl>
2025-05-17 17:52:35 +0200

    init commmit

---
+++ src/main/java/com/example/SVC/SVCApplication.java
@@ -0,0 +1,13 @@
+package com.example.SVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SVCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SVCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/SVC/SVCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SVCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
