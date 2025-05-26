e342a33daca248540634e096ca63a24f33eb92b7
admin . <mikolajsulkowski675@gmail.com>
2025-05-26 10:59:11 +0200

    service

--- src/main/java/com/example/SVC/controller/UserController.java
+++ src/main/java/com/example/SVC/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.SVC.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,15 @@ public class UserController {
     public ResponseEntity<String> createUser(String username, String password) {
         try {
             userService.createUser(username, password);
+            return ResponseEntity.ok("User created successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(999).body("something went wrong");
+        }
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteUser(String username, String password) {
+        try {
+            userService.deleteUser(username, password);
             return ResponseEntity.ok("User created successfully.");
         } catch (Exception e) {
             return ResponseEntity.status(999).body("something went wrong");
